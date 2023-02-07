ff76d09ff8c5822d115d1c7e1d5fb4657503c609
Dayo <105067477+Dy-dayo@users.noreply.github.com>
2023-02-07 17:39:16 +0000

    Initial commit

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>Color Picker Game</h1>
+    <h4>What color do you think will come out 🧐 ?</h4>
+    <div>{{ message }}</div>
+  
+    <button v-for="color in colors" :key="color" @click="matchColor(color)">
+      {{ color }}
+    </button>
+  </template>
+  
+ <script>
+import colorPicker from '../hooks/colorPicker';
+
+export default {
+  name: "ColorPicker",
+  setup(){
+    const {colors, message, matchColor } = colorPicker()
+    return {colors, message, matchColor }
+  }
+};
+</script>
+  
+  <style>
+    body{
+      color:#0E0E6B;
+    }
+    div{
+      margin:5px 5px 15px;
+      
+    }
+    button{
+      color:#0E0E6B;
+      background:#A9A0A0;
+      font-weight:600;
+      display:inline-block;
+      margin:5px;
+      padding:5px 10px;
+      border-radius:6px;
+    }
+  </style>
---
+++ src/hooks/colorPicker.js
@@ -0,0 +1,22 @@
+import { ref } from "@vue/reactivity";
+
+const colorPicker= ()=>{
+    let message = ref("Pick a color...");
+  
+    const colors = ["green", "red", "blue", "purple","Yellow", "Pink"];
+
+      const matchColor = (value) => {
+        // do a random color based on the array index;
+        const randomNumber = Math.floor(Math.random() *5) + 1; //between 1 - 4
+        if (colors[randomNumber] === value) {
+          message.value = `Wow! You got it. You win🥳🥳[answer: ${colors[randomNumber]}]`;
+          return;
+        }
+        message.value = `You guessed wrong😛 😛.  [answer: ${colors[randomNumber]}]`;
+      };
+  
+      return { colors, message, matchColor };
+}
+
+export default colorPicker
+      
